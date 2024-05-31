210f43e230fbdd4650ab898c58aac64eb3959d98
gena <gepota1@.com>
2024-05-31 21:22:46 +0300

    добавили файлы

--- task_1.py
+++ task_1.py
@@ -15,3 +15,53 @@
 не должна быть линейной
 """
 
+from csv import DictReader, DictWriter
+from os.path import exists
+
+def get_info():
+    first_name = "Иван"
+    second_name = "Иванов"
+    phone_number = 89121309238
+    return [first_name, second_name, phone_number]
+
+
+def create_file(file_name):
+    with open(file_name, "w", encoding="utf-8", newline="") as data:
+        f_w = DictWriter(data, fieldnames=["first_name", "secon_name", "phone_number"])
+        f_w.writeheader()
+                
+                
+def write_file(file_name):
+    user_data = get_info()
+    res = read_file(file_name)
+    new_obj = {"first_name": user_data[0], "secon_name": user_data[1], "phone_number": user_data[2]}
+    res.append(new_obj)
+    with open(file_name, "w", encoding="utf-8", newline="") as data:
+        f_w = DictWriter(data, fieldnames=["first_name", "second _name", "phone_number"])
+        f_w.writeheader()
+        f_w.writerows(res)
+       
+
+def read_file(file_name):
+    with open(file_name, encoding= "utf-8") as data:
+        f_r = DictReader(data)
+        return list(f_r) #список со словарями
+    
+file_name = "phone.csv"
+def main():
+    while True:
+        command = input("Введите команду:")
+        if command == "q":
+            break
+        elif command == "w":
+            if not exists(file_name):
+                create_file(file_name)
+            write_file(file_name)
+        elif command == "r":
+            if not exists(file_name):
+                print("Фаил отсутсвует, пожалуйста создайте файл")
+                continue
+            print(*read_file(file_name))
+
+
+main()
