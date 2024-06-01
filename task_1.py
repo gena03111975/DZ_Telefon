9d927a049becc3913ca6741e80f4841c4e84dfbf
gena <gepota1@.com>
2024-06-01 21:47:08 +0300

    Добавили файлы

--- task_1.py
+++ task_1.py
@@ -18,10 +18,27 @@
 from csv import DictReader, DictWriter
 from os.path import exists
 
+class NameError(Exception):
+   def __init__(self, txt):
+        self.txt = txt 
+
 def get_info():
-    first_name = "Иван"
-    second_name = "Иванов"
-    phone_number = 89121309238
+    flag = False
+    while not flag:
+        try:
+            first_name = input("Имя: ")
+            if len(first_name) < 2:
+                raise NameError("Слишком короткое имя")
+            second_name = input("Введите фамилию: ")
+            if len(second_name) < 4:
+                raise NameError("Слишком короткая фамилия")
+            phone_number = input("Ведите номер телефона: ")
+            if len(phone_number) < 11:
+                raise NameError("Слишком короткий номер телефона")
+        except NameError as err:
+            print(err)
+        else:
+            flag = True
     return [first_name, second_name, phone_number]
 
 
@@ -36,17 +53,35 @@ def write_file(file_name):
     res = read_file(file_name)
     new_obj = {"first_name": user_data[0], "secon_name": user_data[1], "phone_number": user_data[2]}
     res.append(new_obj)
-    with open(file_name, "w", encoding="utf-8", newline="") as data:
-        f_w = DictWriter(data, fieldnames=["first_name", "second _name", "phone_number"])
-        f_w.writeheader()
-        f_w.writerows(res)
+    standart_write(file_name, res)
        
 
 def read_file(file_name):
     with open(file_name, encoding= "utf-8") as data:
         f_r = DictReader(data)
         return list(f_r) #список со словарями
-    
+
+
+def remove_row(file_name):
+    search = int(input("Введите номер строки для удаления: "))
+    res = read_file(file_name)
+    if search <= len(res):
+        res.pop(search - 1)
+        standart_write(file_name, res)
+    else:
+        print("Введен неверный номер строки")
+
+
+def standart_write(file_name, res):
+     with open(file_name, "w", encoding="utf-8", newline="") as data:
+        f_w = DictWriter(data, fieldnames=["first_name", "second _name", "phone_number"])
+        f_w.writeheader()
+        f_w.writerows(res)
+
+
+def copy_data():
+
+
 file_name = "phone.csv"
 def main():
     while True:
@@ -62,6 +97,23 @@ def main():
                 print("Фаил отсутсвует, пожалуйста создайте файл")
                 continue
             print(*read_file(file_name))
+        elif command == "d":
+            if not exists(file_name):
+                print("Фаил отсутсвует, пожалуйста создайте файл")
+                continue
+            remove_row(file_name)
+
 
 
 main()
+
+
+
+"""
+реализовать копирование данных из файла А в файл B.
+написать отдельную функцию copy_data:
+прочитать список словарей (read_file)
+и записать его в новый файл используя функцию standart_write
+дополнить функцию main
+из phone.csv в phone2.csv
+"""
